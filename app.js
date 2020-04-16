4403f9ec96aa3b64cdda45f930af7608fbb27a1a
ishvar99 <ishanvarshney99@gmail.com>
2020-04-16 12:49:35 +0530

    setup the database

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const express=require("express")
+const express=require("express");
+const mongoose=require("mongoose");
+const {MONGOURI}=require('./keys');
 const PORT=3000;
 let app=express();
+
+mongoose.connect(MONGOURI, { useNewUrlParser: true,useUnifiedTopology: true} );
+mongoose.connection.on("connected",()=>console.log("Successfully connected to the database!"))
+mongoose.connection.on("error",(err)=>console.log("Failed to connect to database!",err));
 app.get('/',(req,res)=>{
     res.send("WELCOME TO INSTAGRAM")
 })

ecee9f02e91e907366dea862f5246054b2eff58c
ishvar99 <ishanvarshney99@gmail.com>
2020-04-16 23:29:09 +0530

    completed the signup and signin api

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,24 @@ const express=require('express')
 let router=express.Router();
 const mongoose=require('mongoose');
 const User=mongoose.model('User');
+const bcrypt = require('bcryptjs');
 router.get('/',(req,res)=>{
     res.send('Hello from auth')
+})
+router.post('/signin',(req,res)=>{
+    const {email,password}=req.body;
+    if(!email || !password){
+        return res.status(422).json({error:'Complete all the fields'});
+      }
+    User.findOne({email}).then((user)=>{
+        bcrypt.compare(password,user.password).then((response) => {
+            if(!response)
+                return res.status(422).json({error:'Invalid Email or Password!'})
+            res.redirect('/')
+        });
+    }).catch((err)=>{
+        return res.status(422).json({error:'Invalid Email or Password!'});
+    })
 })
 router.post('/signup',(req,res)=>{
     const {name,email,password}=req.body;
@@ -12,12 +28,14 @@ router.post('/signup',(req,res)=>{
         }
       User.findOne({email}).then((savedUser)=>{
           if(savedUser) return res.status(400).json({error:'email already exists'})
-          const user=new User({
-                name,email,password
+          bcrypt.hash(password,12).then((hash)=>{
+            const user=new User({
+                name,email,password:hash
             });
             user.save().then(()=>{
                 res.json({message:'user saved to database'})
             }).catch(err=>res.json({error:err}))
+          });
       }).catch((err)=>console.log(err));
 })
 module.exports=router;

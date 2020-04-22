4f4d2d59498acd7eb652fa0dd5a429564339257c
ishvar99 <ishanvarshney99@gmail.com>
2020-04-22 12:01:20 +0530

    added logout functionality

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,15 +1,22 @@
-import React,{useContext} from 'react';
-import {Link} from 'react-router-dom'
+import React,{useContext, useReducer} from 'react';
+import {Link, useHistory} from 'react-router-dom'
 import {UserContext} from '../App'
 
 const Navbar=()=>{
   const {state,dispatch}=useContext(UserContext);
+  const history=useHistory()
+  const logout=()=>{
+    localStorage.clear()
+    dispatch({type:"CLEAR"})
+    history.push('/signin')
+  }
+  
   const renderList=()=>{
     if(state)
       return [<li><Link to="/profile">Profile</Link></li>,
-      <li><Link to="/create-post">Create Post</Link></li>]
+      <li><Link to="/create-post">Create Post</Link></li>,<a onClick={logout} class="waves-effect waves-light  #42a5f5 blue lighten-1 btn-small">Logout</a>]
     else
-      return [        <li><Link to="/signin">Signin</Link></li>,
+      return [<li><Link to="/signin">Signin</Link></li>,
       <li><Link to="/signup">Signup</Link></li>]
   }
     return (
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -4,5 +4,8 @@ export const userReducer=(state,action)=>{
     if(action.type="USER"){
         return action.payload;
     }
+    else if(action.type="CLEAR"){
+        return null;
+    }
     return state;
 }
